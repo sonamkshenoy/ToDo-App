7165edc9931332965615d5319dfa97759bef7ef1
sonamkshenoy <sonamthemiracle@gmail.com>
2020-06-12 02:49:02 +0530

    Deleting from notComplete, adding to complete and correctly displaying

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -26,33 +26,69 @@ module.exports = function(app){
       return res.redirect('/');
     }    
     // todos = {notCompleted: ['Brushing teeth', 'Washing Car'], completed: ['Iron Clothes','Cook food','Clean room']};
-    todos = {notCompleted: [], completed: ['Iron Clothes','Cook food','Clean room']};
+    todos = {notCompleted: [], completed: []};
     
     
     async function toDisplay(){
       let results = await pool.query('SELECT todoitem FROM notcompleted JOIN users ON (users.userid = notcompleted.userid AND users.emailid = $1)',[sess.email]);
-    // , (error, results)=>{
-        // if(error)
-        // throw error
+      // , (error, results)=>{
+      // if(error)
+      // throw error
 
-    console.log(results.rows);
-        var records = results.rows;
-        await records.forEach(element => {
-          console.log(element['todoitem']);
-          todos.notCompleted.push(element['todoitem']);
-          // console.log(todos);
-        });
-        console.log(todos);
-        res.render('todo', todos);
+      // console.log(results.rows);
+      var records = results.rows;
+      await records.forEach(element => {
+        console.log(element['todoitem']);
+        todos.notCompleted.push(element['todoitem']);
+        // console.log(todos);
+      });
+
+
+      results = await pool.query('SELECT todoitem FROM completed JOIN users ON (users.userid = completed.userid AND users.emailid = $1)',[sess.email]);
+      // console.log(todos);
+      
+      records = results.rows;
+      await records.forEach(element => {
+        console.log(element['todoitem']);
+        todos.completed.push(element['todoitem']);
+        // console.log(todos);
+      });
+
+      res.render('todo', todos);
       // });
     }
     toDisplay();
   })
 
-  // Deleting a todo
+  // Marking a todo complete
   app.post('/mylist', function(req, res){
+    var sess = req.session;
+    
     console.log(req.body.toMarkComplete);
     // one more step left, pop from notCompleted array, long process in JS. Let's try once and for all in Mongo
+    // pool.query('SELECT userid FROM users WHERE emailid = $1', [sess.email], (error, results) => {
+    //   if (error) {
+    //     throw error
+    //   }
+    //   userid = results.rows[0]['userid'];
+    //   console.log("Query result",results.rows[0]['userid']);
+    //   pool.query('INSERT INTO completed VALUES ($1, $2)', [userid, req.body.toMarkComplete], (error, results) => {
+    //     if (error) {
+    //       throw error
+    //     }
+    //     console.log("insertiong to todos successful");
+    //   });
+    // });
+
+
+    async function toMarkCompleteTodo(){
+      var results = await pool.query('SELECT userid FROM users WHERE emailid = $1', [sess.email]);
+      var userid = results.rows[0]['userid'];
+      console.log("Query result",results.rows[0]['userid']);
+      result = await pool.query('INSERT INTO completed VALUES ($1, $2)', [userid, req.body.toMarkComplete]);
+      result = await pool.query('DELETE FROM notcompleted WHERE todoitem = $1', [req.body.toMarkComplete]);
+    }
+    toMarkCompleteTodo();
     res.status(200).send("Successful Application");
   })
 
