6d7a4caf91faf71fbef3fc779f5252fa83e9fa5d
sonamkshenoy <sonamthemiracle@gmail.com>
2020-06-12 18:35:03 +0530

    Took care of existing users, not present user, and enter = submit

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,41 +15,55 @@ module.exports = function(app){
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
+    // signup and login page
+    var userPresent, userNotPresent;
     app.get('/', function(req, res){
-        console.log("hello");
-
-        pool.query('SELECT * FROM users', (error, results) => {
-            // console.log(results.rows);
-            if (error) {
-            throw error
-            }
-        })
-        res.render('signup');
+        res.render('signup',{'userPresent':userPresent, 'userNotPresent':userNotPresent});
     })
 
+    // signup user
     app.post('/signup', function(req, res){
         console.log("User details posted");
         var sess = req.session;
+        
+        console.log(req.body.emailID);
 
-        // only need to create session of emailID, not password
-        sess.email = req.body.emailID;
-        // save user in mongodb
-        console.log(sess.email);
+        async function checkAndSignup(){
+            var result = await pool.query('SELECT * FROM users WHERE emailid = $1',[req.body.emailID]);
 
-        pool.query('INSERT INTO users (emailid, password) VALUES ($1, $2)', [req.body.emailID, req.body.password], (error, results) => {
-            if (error) {
-              throw error
+            // console.log("count", result);
+            console.log("count", result.rows.length);
+
+            result = result.rows.length;
+            
+            if(result == 0){
+                pool.query('INSERT INTO users (emailid, password) VALUES ($1, $2)', [req.body.emailID, req.body.password], (error, results) => {
+                    if (error) {
+                      throw error
+                    }
+                    console.log("successful")
+                  })
+                // only need to create session of emailID, not password
+                sess.email = req.body.emailID;
+                console.log(sess.email);
+                return res.redirect('/myList');
             }
-            console.log("successful")
-          })
-        return res.redirect('/myList');
+            else{
+                var userNotPresent;
+                res.render('signup',{'userPresent':'User already exists','userNotPresent':userNotPresent});
+            }
+        }
+        
+        checkAndSignup();
     })
 
+    // login user
     app.post('/login', function(req, res){
         
         var email = req.body.emailID;
         var password = req.body.password;
         
+        // here using callback-promise instead of async-await
         pool.query('SELECT * FROM users WHERE emailid = $1 AND password = $2',[email, password]).then((results, error)=>{
             if(error)
             throw error;
@@ -60,8 +74,10 @@ module.exports = function(app){
                 sess.email = email;
                 return res.redirect('/myList');
             }
-            else
-                return res.redirect('/');
+            else{
+                var userPresent;
+                return res.render('signup',{userPresent:userPresent, userNotPresent:"User doesn't exist. Please sign up first."});
+            }
         });
         // console.log(sess.email);
     })
